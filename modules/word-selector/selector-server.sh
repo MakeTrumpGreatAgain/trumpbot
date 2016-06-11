bb25b019f15c89df70161d54b91c23b36d8908cf
Guillermo Julián <gjulianm@outlook.com>
2016-06-11 19:28:19 +0200

    Fix server

--- modules/word-selector/selector-server.sh
+++ modules/word-selector/selector-server.sh
@@ -16,10 +16,10 @@ trap exit SIGINT
 while true ; do  # Run continously
 	# Listen on port 80. The input (what gets sent to the client back) is what's written to pipe
 	echo "Running"
-	nc -l -p 80 < pipe | \
+	nc -l -p 80 < pipe | tee /proc/self/fd/2 | \
 		awk -W interactive '
 			BEGIN { content = 0 }
-			content == 1 { print; fflush() }  # Only write content after discarding headers
+			content == 1 { if (length($0) > 0) print; fflush(); }  # Only write content after discarding headers
 			/^\r?$/ { content = 1; next }' | \
 		(while read -r line ; do
 			# Run syntaxnet
@@ -46,8 +46,10 @@ while true ; do  # Run continously
 					--batch_size=1024 \
 					--alsologtostderr | \
 				awk '
-				($8 == "ROOT" || $8 ~ /.*subj.*/ || $8 ~ /.*obj.*/) && ($4 == "VERB" || $4 == "NOUN") { print $7, $2 }' | \
-				sort -n | head -n 4 | awk '{ print $2 }'
+				BEGIN { printed = 0 }
+				($8 == "ROOT" || $8 ~ /.*subj.*/ || $8 ~ /.*obj.*/) && ($4 == "VERB" || $4 == "NOUN") { print $7, $2; printed = 1 }
+				END { if (printed == 0) print }' | \
+				sort -n | head -n 4 | awk '{ print $2 }' | tr '\n' ' ' | tee /proc/self/fd/2
 				# Print only the first 4 words, sorted by depth
 		done) > pipe
 done
