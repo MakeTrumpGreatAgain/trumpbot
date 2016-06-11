04f53b4deab38bdcda358f302934ab73e83eab44
Guillermo Julián <gjulianm@outlook.com>
2016-06-11 17:12:00 +0200

    Shameful implementation of the server

---
+++ modules/word-selector/selector-server.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Note: I purposedly wrote this crap in awk, bash and more to see what can one
+# do with such basic tools.
+
+# Create a pipe to "loop" the input, so the final print can be brought back to the
+# client with netcat
+mkfifo pipe
+
+# Configuration for the syntaxnet program
+PARSER_EVAL=bazel-bin/syntaxnet/parser_eval
+MODEL_DIR=syntaxnet/models/parsey_mcparseface
+INPUT_FORMAT=stdin
+
+trap exit SIGINT
+
+while true ; do  # Run continously
+	# Listen on port 80. The input (what gets sent to the client back) is what's written to pipe
+	echo "Running"
+	nc -l -p 80 < pipe | \
+		awk -W interactive '
+			BEGIN { content = 0 }
+			content == 1 { print; fflush() }  # Only write content after discarding headers
+			/^\r?$/ { content = 1; next }' | \
+		(while read -r line ; do
+			# Run syntaxnet
+			echo $line | $PARSER_EVAL \
+					--input=$INPUT_FORMAT \
+					--output=stdout-conll \
+					--hidden_layer_sizes=64 \
+					--arg_prefix=brain_tagger \
+					--graph_builder=structured \
+					--task_context=$MODEL_DIR/context.pbtxt \
+					--model_path=$MODEL_DIR/tagger-params \
+					--slim_model \
+					--batch_size=1024 \
+					--alsologtostderr | \
+				$PARSER_EVAL \
+					--input=stdin-conll \
+					--output=stdout-conll \
+					--hidden_layer_sizes=512,512 \
+					--arg_prefix=brain_parser \
+					--graph_builder=structured \
+					--task_context=$MODEL_DIR/context.pbtxt \
+					--model_path=$MODEL_DIR/parser-params \
+					--slim_model \
+					--batch_size=1024 \
+					--alsologtostderr | \
+				awk '
+				($8 == "ROOT" || $8 ~ /.*subj.*/ || $8 ~ /.*obj.*/) && ($4 == "VERB" || $4 == "NOUN") { print $7, $2 }' | \
+				sort -n | head -n 4 | awk '{ print $2 }'
+				# Print only the first 4 words, sorted by depth
+		done) > pipe
+done
